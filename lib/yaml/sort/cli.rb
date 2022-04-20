a9c05a4c1375190d5fc61c3a127b29c35649e3a7
Romain Tartière <romain@blogreen.org>
2022-04-19 16:37:47 -1000

    Add in-place editing capability

--- lib/yaml/sort/cli.rb
+++ lib/yaml/sort/cli.rb
@@ -12,26 +12,65 @@ module Yaml
       end
 
       def execute(argv) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-        options = {}
+        options = {
+          in_place: false,
+        }
         OptionParser.new do |opts|
           opts.banner = "Usage: yaml-sort [options] [filename]"
 
           opts.on("-d", "--[no-]debug", "Run verbosely") do |d|
             options[:debug] = d
           end
+          opts.on("-i", "--in-place", "Update files in-place") do
+            options[:in_place] = true
+          end
         end.parse!
-        io = argv.empty? ? $stdin.read : File.read(argv.shift)
 
-        document = @parser.parse(io)
-        document = document.sort
-
-        if options[:debug]
-          pp document
-          exit
+        if !options[:in_place] && argv.count > 1
+          warn "Sorting multiple YAML document to stdout does not make sense"
+          exit 1
         end
 
-        puts "---"
-        puts document.to_s
+        if options[:in_place] && argv.count < 1
+          warn "Cannot sort in-place when reading from stdin"
+          exit 1
+        end
+
+        if argv.empty?
+          process_document(nil, options)
+        else
+          argv.each do |filename|
+            process_document(filename, options)
+          end
+        end
+      end
+
+      def process_document(filename, options)
+        yaml = read_document(filename)
+        yaml = sort_yaml(yaml)
+        write_output(yaml, filename, options)
+      end
+
+      def read_document(filename)
+        if filename
+          File.read(filename)
+        else
+          $stdin.read
+        end
+      end
+
+      def sort_yaml(yaml)
+        document = @parser.parse(yaml)
+        document = document.sort
+        "---\n#{document}\n"
+      end
+
+      def write_output(yaml, filename, options)
+        if options[:in_place]
+          File.write(filename, yaml)
+        else
+          puts yaml
+        end
       end
     end
   end
